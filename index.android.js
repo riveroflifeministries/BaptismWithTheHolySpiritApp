56dbab7917709cf703973058d41dc646ec32320c
Izak Filmalter <izak@whoatech.com>
2016-08-22 17:06:15 -0700

    Add in material ui for Android. Get initial android version up and running.

--- index.android.js
+++ index.android.js
@@ -1,52 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Navigator, View, StatusBar } from 'react-native';
+import { Toolbar } from 'react-native-material-ui';
+
+import FathersPromise from './content/FathersPromise';
 
 class BaptismWithTheHolySpiritApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View>
+        <StatusBar
+          backgroundColor="#1976D2"
+          barStyle="light-content" />
+
+        <Toolbar
+          title="The Fathers Promise" />
+
+        <FathersPromise />
       </View>
-    );
+
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  toolbar: {
+    backgroundColor: 'grey',
+    height: 40,
   },
 });
 
