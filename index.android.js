9756c8e699cb846174c65aa9601a76fdb597283d
Izak Filmalter <izak@whoatech.com>
2016-08-31 12:45:56 -0700

    Get simplified app ready for initial beta.

--- index.android.js
+++ index.android.js
@@ -1,85 +1,21 @@
 'use strict';
 
-import React, { Component, PropTypes } from 'react';
-import { AppRegistry, StyleSheet, Navigator, DrawerLayoutAndroid, ScrollView, View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, StatusBar, StyleSheet, View } from 'react-native';
+import { Toolbar } from 'react-native-material-ui';
+import FathersPromise from './src/content/FathersPromise';
 
-import Navigate from './src/utils/Navigate';
-import Toolbar from './src/components/Toolbar';
-import Navigation from './src/scenes/Navigation';
-import Home from './src/scenes/Home';
 
 class BaptismWithTheHolySpiritApp extends Component {
-
-  static childContextTypes = {
-    drawer: React.PropTypes.object,
-    navigator: React.PropTypes.object
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      drawer: null,
-      navigator: null
-    };
-  }
-
-  getChildContext = () => {
-    return {
-      drawer: this.state.drawer,
-      navigator: this.state.navigator
-    }
-  };
-
-  setDrawer = (drawer) => {
-    this.setState({
-      drawer
-    });
-  };
-
-  setNavigator = (navigator) => {
-    this.setState({
-      navigator: new Navigate(navigator)
-    });
-  };
-
   render() {
-    const { drawer, navigator } = this.state;
-    const navView = React.createElement(Navigation);
-
     return (
-      <DrawerLayoutAndroid
-        drawerWidth={300}
-        drawerPosition={DrawerLayoutAndroid.positions.Left}
-        renderNavigationView={() => {
-          if (drawer && navigator) {
-            return navView;
-          }
-          return null;
-        }}
-        ref={(drawer) => { !this.state.drawer ? this.setDrawer(drawer) : null }}
-      >
-        {drawer &&
-        <Navigator
-          initialRoute={Navigate.getInitialRoute()}
-          configureScene={() => {
-            return Navigator.SceneConfigs.FadeAndroid;
-          }}
-          ref={(navigator) => { !this.state.navigator ? this.setNavigator(navigator) : null }}
-          renderScene={(route) => {
-            if (this.state.navigator && route.component) {
-              return (
-                <View
-                  style={styles.container}
-                  showsVerticalScrollIndicator={false}>
-                  <Toolbar onIconPress={drawer.openDrawer} />
-                  <route.component title={route.title} path={route.path} {...route.props} />
-                </View>
-              );
-            }
-          }}
-        />
-        }
-      </DrawerLayoutAndroid>
+      <View
+        style={styles.container}
+        showsVerticalScrollIndicator={false}>
+        <StatusBar backgroundColor='#1976D2' />
+        <Toolbar title='The Fathers Promise' />
+        <FathersPromise />
+      </View>
     );
   }
 }
